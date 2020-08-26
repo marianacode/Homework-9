f64aa28c7e7f6e7c0678ac1e14cf1b22bd4c3a95
Mariana Robertson <marygmaciel@gmail.com>
2020-08-25 20:04:47 -0700

    Updated index.js and README

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
-README Generator
+# 
 
-A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
+## Description 
 
-A command-line application that dynamically generates a README.md from a user's input. The application will be invoked with the following command:
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Contributors](#contributors)
+- [Details](#details)
+## Installation
+Packages required to run this program are: 
 
-```
-node index.js
-```
+## Usage
+Examples of how to use this program: undefined
+## License
+APACHE 2.0
+## Tests
+To test, run the following command: 
+## Contributors
 
+## Contact
+
+![Badge](https://img.shields.io/badge/Github--4cbbb9) 
+
+![Profile Image](https://github.com/.png?size=50)
+
+View the project in GitHub at: undefined
+
+If you have any questions, contact the author directly at .
--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function askQuestions() {
             name: "email"
         },
 
+
         {
             type: "input",
             message: "What is the title of your project?",
@@ -34,8 +35,14 @@ function askQuestions() {
 
         {
             type: "input",
+            message: "What packages need to be installed to run your project?",
+            name: "installation"
+        },
+
+        {
+            type: "input",
             message: "Provide a description of your project",
-            name: "udescription"
+            name: "description"
         },
 
         {
@@ -45,9 +52,29 @@ function askQuestions() {
         },
 
         {
+            type: "list",
+            name: "license",
+            message: "What kind of license would you like to have?",
+            name: "license",
+            choices: [ "APACHE 2.0", "GPL v3", "BSD 3", "None"]
+          },
+
+          {
+            type: "input",
+            message: "Please list all contributors?",
+            name: "contributer"
+        },
+
+        {
             type: "input",
             message: "Provide the URL of your project?",
             name: "URL"
+        },
+
+        {
+            type: "input",
+            message: "What command is used to run a test?",
+            name: "tests"
         },
 
        
